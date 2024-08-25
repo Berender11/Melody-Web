51877129915569b7b6cc8685cf7741c2b4c73177
Blaze1104 <berendery1@gmail.com>
2024-08-25 16:41:38 +0530

    Added Volume button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Initializing Variables
 let songIndex = 0;
-let audioElement = new Audio("songs/1.mp3");
-audioElement.volume = 0.1;
+let volumeCount = 1;
+let audioElement = new Audio("songs/acousticbreeze.mp3");
 let masterPlay = document.getElementById("masterPlay");
 let previousBtn = document.getElementById("previousBtn");
 let nextBtn = document.getElementById("nextBtn");
@@ -10,17 +10,22 @@ let gif = document.getElementById("gif");
 let masterSongName = document.getElementById("masterSongName");
 let songItems = Array.from(document.getElementsByClassName("songItem"));
 let songListIcon = Array.from(document.getElementsByClassName("songListIcon"));
+let volumeBtn = document.querySelector(".volumeIcon");
+let volumeValue = document.querySelector("#volume");
 
 //Song List
 let songs = [
-    { songName: "Salam-e-ishq1", filePath: "songs/1.mp3", coverPath: "covers/1.jpg", duration: "2:30" },
-    { songName: "Salam-e-ishq2", filePath: "songs/2.mp3", coverPath: "covers/2.jpg", duration: "3:15" },
-    { songName: "Salam-e-ishq3", filePath: "songs/3.mp3", coverPath: "covers/3.jpg", duration: "4:20" },
-    { songName: "Salam-e-ishq4", filePath: "songs/4.mp3", coverPath: "covers/4.jpg", duration: "2:00" },
-    { songName: "Salam-e-ishq5", filePath: "songs/5.mp3", coverPath: "covers/5.jpg", duration: "1:40" },
-    { songName: "Salam-e-ishq6", filePath: "songs/6.mp3", coverPath: "covers/6.jpg", duration: "4:10" },
-    { songName: "Salam-e-ishq7", filePath: "songs/7.mp3", coverPath: "covers/7.jpg", duration: "3:30" },
+    { songName: "Acousticbreeze", filePath: "songs/acousticbreeze.mp3", coverPath: "covers/acousticbreeze.jpg", duration: "2:30" },
+    { songName: "Creativeminds", filePath: "songs/creativeminds.mp3", coverPath: "covers/creativeminds.jpg", duration: "3:15" },
+    { songName: "Echoing Streets", filePath: "songs/echoing-streets.mp3", coverPath: "covers/echoing-streets.jpg", duration: "4:20" },
+    { songName: "Elektronomia", filePath: "songs/Elektronomia - Energy.mp3", coverPath: "covers/Elektronomia - Energy.jpg", duration: "2:00" },
+    { songName: "Jazzcomedy", filePath: "songs/jazzcomedy.mp3", coverPath: "covers/jazzcomedy.jpg", duration: "1:40" },
+    { songName: "Slow-burn", filePath: "songs/Kevin-Slow-burn.mp3", coverPath: "covers/Kevin-Slow-burn.jpg", duration: "4:10" },
+    { songName: "Ukulele", filePath: "songs/ukulele.mp3", coverPath: "covers/ukulele.jpg", duration: "3:30" },
 ]
+
+masterSongName.innerText = songs[0].songName;
+audioElement.volume = 0.2;
 
 songItems.forEach((element, i) => {
     element.getElementsByTagName("img")[0].src = songs[i].coverPath;
@@ -63,7 +68,7 @@ let previousSong = () => {
     else {
         songIndex -= 1;
     }
-    audioElement.src = `songs/${songIndex+1}.mp3`;
+    audioElement.src = songs[songIndex].filePath;
     masterSongName.innerText = songs[songIndex].songName;
     audioElement.currentTime = 0;
     audioElement.play();
@@ -78,12 +83,26 @@ let nextSong = () => {
     else {
         songIndex += 1;
     }
-    audioElement.src = `songs/${songIndex+1}.mp3`;
+    audioElement.src = songs[songIndex].filePath;
     masterSongName.innerText = songs[songIndex].songName;
     audioElement.currentTime = 0;
     audioElement.play();
     gif.style.opacity = 1;
     masterPlay.src = "icons/pause-solid.svg";
+}
+
+let volumeControl = () => {
+    if (volumeCount == 1) {
+        audioElement.volume = 0;
+        volumeValue.value = 0;
+        volumeBtn.src = "icons/volume-xmark-solid.svg";
+        volumeCount = 0;
+    } else {
+        audioElement.volume = 0.2;
+        volumeBtn.src = "icons/volume-high-solid.svg";
+        volumeValue.value = 0.2;
+        volumeCount = 1;
+    }
 }
 
 masterPlay.addEventListener("click", playPauseSong);
@@ -92,12 +111,22 @@ audioElement.addEventListener("timeupdate", songbarUpdate);
 
 myProgressBar.addEventListener("change", songSync);
 
+volumeValue.addEventListener("change", ()=>{
+    if (volumeValue.value == 0) {
+        volumeBtn.src = "icons/volume-xmark-solid.svg";
+        audioElement.volume = 0;
+    } else {
+        volumeBtn.src = "icons/volume-high-solid.svg";
+        audioElement.volume = volumeValue.value;
+    }
+});
+
 songListIcon.forEach((element) => {
     element.addEventListener("click", (e) => {
         makeAllPlay();
         songIndex = parseInt(e.target.id);
         e.target.src = "icons/pause-solid.svg";
-        audioElement.src = `songs/${songIndex+1}.mp3`;
+        audioElement.src = songs[songIndex].filePath;
         masterSongName.innerText = songs[songIndex].songName;
         audioElement.currentTime = 0;
         audioElement.play();
@@ -108,3 +137,4 @@ songListIcon.forEach((element) => {
 
 previousBtn.addEventListener("click", previousSong);
 nextBtn.addEventListener("click", nextSong);
+volumeBtn.addEventListener("click", volumeControl);
