88865a20ca838ce923f1f24bd119bf5c0bc0860d
Blaze1104 <berendery1@gmail.com>
2025-06-28 22:19:17 +0530

    Made the songs loading Dynamic

--- script.js
+++ script.js
@@ -1,140 +1,92 @@
-// Initializing Variables
-let songIndex = 0;
-let volumeCount = 1;
-let audioElement = new Audio("songs/acousticbreeze.mp3");
-let masterPlay = document.getElementById("masterPlay");
-let previousBtn = document.getElementById("previousBtn");
-let nextBtn = document.getElementById("nextBtn");
-let myProgressBar = document.getElementById("songBar");
-let gif = document.getElementById("gif");
-let masterSongName = document.getElementById("masterSongName");
-let songItems = Array.from(document.getElementsByClassName("songItem"));
-let songListIcon = Array.from(document.getElementsByClassName("songListIcon"));
-let volumeBtn = document.querySelector(".volumeIcon");
-let volumeValue = document.querySelector("#volume");
+const API_URL = "https://api.jamendo.com/v3.0/tracks/?client_id=f42ab926&format=json&limit=7&audioformat=mp32";
 
-//Song List
-let songs = [
-    { songName: "Acousticbreeze", filePath: "songs/acousticbreeze.mp3", coverPath: "covers/acousticbreeze.jpg", duration: "2:30" },
-    { songName: "Creativeminds", filePath: "songs/creativeminds.mp3", coverPath: "covers/creativeminds.jpg", duration: "3:15" },
-    { songName: "Echoing Streets", filePath: "songs/echoing-streets.mp3", coverPath: "covers/echoing-streets.jpg", duration: "4:20" },
-    { songName: "Elektronomia", filePath: "songs/Elektronomia - Energy.mp3", coverPath: "covers/Elektronomia - Energy.jpg", duration: "2:00" },
-    { songName: "Jazzcomedy", filePath: "songs/jazzcomedy.mp3", coverPath: "covers/jazzcomedy.jpg", duration: "1:40" },
-    { songName: "Slow-burn", filePath: "songs/Kevin-Slow-burn.mp3", coverPath: "covers/Kevin-Slow-burn.jpg", duration: "4:10" },
-    { songName: "Ukulele", filePath: "songs/ukulele.mp3", coverPath: "covers/ukulele.jpg", duration: "3:30" },
-]
+const masterPlay = document.getElementById("masterPlay");
+const progressBar = document.getElementById("songBar");
+const gif = document.getElementById("gif");
+const masterSongName = document.getElementById("masterSongName");
+const volumeControl = document.getElementById("volume");
 
-masterSongName.innerText = songs[0].songName;
-audioElement.volume = 0.2;
+let audio = new Audio();
+let currentIndex = -1;
+let songList = [];
 
-songItems.forEach((element, i) => {
-    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
-    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
-    element.getElementsByClassName("timeStamp")[0].innerHTML = songs[i].duration;
-})
-//FUNCTIONS
-let playPauseSong = () => {
-    if (audioElement.paused || audioElement.currentTime <= 0) {
-        audioElement.play();
-        masterPlay.src = "icons/pause-solid.svg";
-        gif.style.opacity = 1;
-    }
-    else {
-        audioElement.pause();
-        masterPlay.src = "icons/play-solid.svg";
-        gif.style.opacity = 0;
-    }
-};
+async function loadSongs() {
+  try {
+    const res = await fetch(API_URL);
+    const data = await res.json();
+    songList = data.results;
+    
+    const container = document.getElementById("songListContainer");
 
-let songSync = () => {
-    audioElement.currentTime = (myProgressBar.value * audioElement.duration) / 100;
-};
+    songList.forEach((song, index) => {
+      const songDiv = document.createElement("div");
+      songDiv.className = "songItem";
+      songDiv.innerHTML = `
+        <img src="${song.album_image}" class="bannerImg" alt="cover">
+        <span class="songName">${song.name}</span>
+        <span class="songlistplay">
+          <span class="timeStamp">${Math.floor(song.duration / 60)}:${("0" + (song.duration % 60)).slice(-2)}</span>
+          <span><img id="play-${index}" class="songListIcon" src="icons/play-solid.svg" alt="Play"></span>
+        </span>
+      `;
+      container.appendChild(songDiv);
 
-let songbarUpdate = () => {
-    let progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
-    myProgressBar.value = progress;
+      const playButton = document.getElementById(`play-${index}`);
+      playButton.addEventListener("click", () => {
+        playSong(index);
+      });
+    });
+  } catch (err) {
+    console.error("Error loading songs:", err);
+  }
 }
 
-const makeAllPlay = () => {
-    Array.from(document.getElementsByClassName("songListIcon")).forEach((element) => {
-        element.src = "icons/play-solid.svg";
+function playSong(index) {
+  const song = songList[index];
+  if (!song) return;
+
+  audio.src = song.audio;
+  audio.load(); // reload in case src is the same
+  audio.volume = volumeControl.value;
+  audio.play()
+    .then(() => {
+      currentIndex = index;
+      masterSongName.textContent = `${song.name} - ${song.artist_name}`;
+      gif.style.opacity = 1;
+      masterPlay.src = "icons/pause-solid.svg";
     })
-};
-
-let previousSong = () => {
-    if (songIndex <= 0) {
-        songIndex = 0;
-    }
-    else {
-        songIndex -= 1;
-    }
-    audioElement.src = songs[songIndex].filePath;
-    masterSongName.innerText = songs[songIndex].songName;
-    audioElement.currentTime = 0;
-    audioElement.play();
-    gif.style.opacity = 1;
-    masterPlay.src = "icons/pause-solid.svg";
+    .catch(err => {
+      console.error("Audio play failed:", err);
+      alert("Playback failed. Jamendo might be blocking the stream or browser autoplay is preventing it.");
+    });
 }
 
-let nextSong = () => {
-    if (songIndex >= 6) {
-        songIndex = 0;
-    }
-    else {
-        songIndex += 1;
-    }
-    audioElement.src = songs[songIndex].filePath;
-    masterSongName.innerText = songs[songIndex].songName;
-    audioElement.currentTime = 0;
-    audioElement.play();
-    gif.style.opacity = 1;
-    masterPlay.src = "icons/pause-solid.svg";
-}
-
-let volumeControl = () => {
-    if (volumeCount == 1) {
-        audioElement.volume = 0;
-        volumeValue.value = 0;
-        volumeBtn.src = "icons/volume-xmark-solid.svg";
-        volumeCount = 0;
-    } else {
-        audioElement.volume = 0.2;
-        volumeBtn.src = "icons/volume-high-solid.svg";
-        volumeValue.value = 0.2;
-        volumeCount = 1;
-    }
-}
-
-masterPlay.addEventListener("click", playPauseSong);
-
-audioElement.addEventListener("timeupdate", songbarUpdate);
-
-myProgressBar.addEventListener("change", songSync);
-
-volumeValue.addEventListener("change", ()=>{
-    if (volumeValue.value == 0) {
-        volumeBtn.src = "icons/volume-xmark-solid.svg";
-        audioElement.volume = 0;
-    } else {
-        volumeBtn.src = "icons/volume-high-solid.svg";
-        audioElement.volume = volumeValue.value;
-    }
+masterPlay.addEventListener("click", () => {
+  if (audio.paused || audio.currentTime <= 0) {
+    audio.play().then(() => {
+      masterPlay.src = "icons/pause-solid.svg";
+      gif.style.opacity = 1;
+    });
+  } else {
+    audio.pause();
+    masterPlay.src = "icons/play-solid.svg";
+    gif.style.opacity = 0;
+  }
 });
 
-songListIcon.forEach((element) => {
-    element.addEventListener("click", (e) => {
-        makeAllPlay();
-        songIndex = parseInt(e.target.id);
-        e.target.src = "icons/pause-solid.svg";
-        audioElement.src = songs[songIndex].filePath;
-        masterSongName.innerText = songs[songIndex].songName;
-        audioElement.currentTime = 0;
-        audioElement.play();
-        gif.style.opacity = 1;
-        masterPlay.src = "icons/pause-solid.svg";
-    })
+audio.addEventListener("timeupdate", () => {
+  const progress = parseInt((audio.currentTime / audio.duration) * 100);
+  progressBar.value = progress || 0;
 });
 
-previousBtn.addEventListener("click", previousSong);
-nextBtn.addEventListener("click", nextSong);
-volumeBtn.addEventListener("click", volumeControl);
+progressBar.addEventListener("change", () => {
+  if (!isNaN(audio.duration) && audio.duration > 0) {
+    audio.currentTime = (progressBar.value * audio.duration) / 100;
+  }
+});
+
+volumeControl.addEventListener("input", () => {
+  audio.volume = volumeControl.value;
+});
+
+loadSongs();
